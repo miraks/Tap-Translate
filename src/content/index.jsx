a322bb7679665bf90713eb438eef3ab7b31d5eda
miraks <a@vldkn.net>
2018-09-27 17:22:43 +0300

    Use shadow DOM when possible to avoid overlap with page styles

--- src/content/index.jsx
+++ src/content/index.jsx
@@ -1,5 +1,6 @@
 import { render, h } from 'preact'
 import '@/content/styles/index.sass'
+import shadowStyles from '!to-string-loader!css-loader!sass-loader!@/content/styles/shadow.sass'
 import debounce from '@/helpers/debounce'
 import Main from '@/content/components/Main'
 
@@ -35,7 +36,16 @@ const init = () => {
   const root = document.createElement('div')
   root.id = 'tap-translate'
   document.body.appendChild(root)
-  render(<Main selectionListener={selectionListener} storageListener={storageListener}/>, root)
+
+  let shadow = null
+  if (root.attachShadow) {
+    shadow = root.attachShadow({ mode: 'open' })
+    const style = document.createElement('style')
+    style.textContent = shadowStyles
+    shadow.appendChild(style)
+  }
+
+  render(<Main selectionListener={selectionListener} storageListener={storageListener}/>, shadow || root)
 
   const observer = new MutationObserver(() => {
     if (document.body.lastElementChild === root) return
